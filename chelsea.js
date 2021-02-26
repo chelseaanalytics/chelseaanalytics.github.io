d030f0c9d5e5d297d4c98d755d8c4c52a39b1360
chelseaanalytics <79656978+chelseaanalytics@users.noreply.github.com>
2021-02-26 10:08:49 -0500

    Update chelsea.js

--- chelsea.js
+++ chelsea.js
@@ -1,3 +1,52 @@
+function draw() {
+
+//Draw random fills
+a = Math.random()*rectWidth;
+a = Math.round(a/size)*size;
+b = Math.random()*rectHeight;
+b = Math.round(b/size)*size;
+
+if((a+size)<=rectWidth && (b+size)<=rectHeight){
+  gl.fillStyle = '#064276';  //#073156
+  gl.fillRect(a, b, size, size);
+  gl.fillStyle = '#0E4778';  //#073156
+  gl.fillRect(a-size, b-size, size, size);
+  gl.fillStyle = '#073156';  //#073156 #CBE3F8
+  gl.fillRect(a, b-size, size, size);
+  //gl.fillStyle = '#CBE3F8';  //#073156 #CBE3F8
+  //gl.fillRect(a-size, b, size, size);
+  gl.strokeStyle = "#064276";
+  gl.lineWidth = 1;
+  gl.rect(a, b, size, size);
+  gl.stroke();
+  gl.save
+}
+else {
+  a = 0;
+  b = 0;
+}
+
+if (count<10000) {
+  count = count+1;
+}
+else {
+  window.clearInterval(animator);
+}
+
+// Draw text
+gl.fillStyle = "white";
+if (width > 800) {
+  gl.font = '72px Helvetica';
+}
+else {
+  gl.font = '36px Helvetica';
+}
+gl.textAlign = 'center';
+gl.fillText('Chelsea Analytics', width/2, height/2);
+
+}
+
+// *******************   Main program   ******************* //
 const canvas = document.querySelector("Canvas");
 var gl = document.getElementById('Canvas').getContext('2d');
 
@@ -10,127 +59,21 @@ var count = 0;
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
-function draw() {
+rectWidth = Math.floor((width/size))*size+buffer*2;
+rectHeight = Math.floor((height/size))*size+buffer*2;
 
-  rectWidth = Math.floor((width/size))*size+buffer*2;
-  rectHeight = Math.floor((height/size))*size+buffer*2;
-
-// Draw canvas, but only first time through
-  if ((x === 0) && (y === 0)){
-    gl.clearRect(0, 0, rectWidth, rectHeight);
-    gl.fillStyle = '#6498C6';
-    gl.fillRect(0, 0, rectWidth, rectHeight);
-  }
+// Draw canvas
+gl.clearRect(0, 0, rectWidth, rectHeight);
+gl.fillStyle = '#BFDDF6';
+gl.fillRect(0, 0, rectWidth, rectHeight);
 
 // Draw background
-  if ((x+size)<=rectWidth) {
-    if ((y+size)<=rectHeight) {
-      gl.fillStyle = '#BFDDF6';
-      gl.fillRect(x, y, size, size);
-      gl.strokeStyle = "white";
-      gl.lineWidth = 1;
-      gl.rect(x, y, size, size);
-      gl.stroke();
-      //gl.save();
-      y += size;
-    }
-    else {
-      y = 0;
-      x += size;
-    }
-  }
-  //else {
-
-  //}
-
- //Draw random fills
-  a = Math.random()*rectWidth;
-  a = Math.round(a/size)*size;
-  b = Math.random()*rectHeight;
-  b = Math.round(b/size)*size;
-
-  if((a+size)<=rectWidth && (b+size)<=rectHeight){
-    gl.fillStyle = '#064276';  //#073156
-    gl.fillRect(a, b, size, size);
-    gl.fillStyle = '#0E4778';  //#073156
-    gl.fillRect(a-size, b-size, size, size);
-    gl.fillStyle = '#073156';  //#073156 #CBE3F8
-    gl.fillRect(a, b-size, size, size);
-    //gl.fillStyle = '#CBE3F8';  //#073156 #CBE3F8
-    //gl.fillRect(a-size, b, size, size);
-    gl.strokeStyle = "#064276";
-    gl.lineWidth = 1;
-    gl.rect(a, b, size, size);
+for (x=0;x<rectWidth;x+=size) {
+  for (y=0;y<=rectHeight;y+=size) {
+    gl.rect(x, y, size, size);
     gl.stroke();
-    gl.save
   }
-  else {
-    a = 0;
-    b = 0;
-  }
-
-
-
-
-  //Animate
-  window.setInterval(draw, 20);
-  // Draw text
-  gl.fillStyle = "white";
-  if (width > 800) {
-    gl.font = '72px Helvetica';
-  }
-  else {
-    gl.font = '36px Helvetica';
-  }
-  gl.textAlign = 'center';
-  gl.fillText('Chelsea Analytics', width/2, height/2);
-  gl.restore();
-
-
-
-  canvas.addEventListener("mousemove", on_mousemove, false);
-  canvas.addEventListener("click", on_click, false);
-
 }
 
-draw();
-
-
-  //for(i=0;i<=rectWidth;i+=size) {
-  //  for (j=0;j<=rectHeight;j+=size) {
-  //if(((buffer+i+size)<=(rectWidth)) && ((buffer+j+size)<=(rectHeight))) {
-    //gl.rect(20, 20+i, 100, 100);
-    //if((i+j)%3 === 0){
-      //gl.fillStyle = '#BFDDF6';
-      //gl.fillRect(buffer+i, buffer+j, size, size);
-      //gl.save();
-    //}
-    //x = Math.floor(Math.random()*rectWidth);
-    //y = Math.floor(Math.random()*rectHeight);
-    //if(((x+size)<rectWidth) && (y+size)<rectHeight){
-      //gl.fillStyle = '#073156';  //#073156
-      //gl.fillRect(x, y, size, size);
-      //gl.fillStyle = '#064276';  //#073156
-      //gl.fillRect(x-size, y-size, size, size);
-      //gl.save
-    //}
-    //gl.strokeStyle = "white";
-    //gl.lineWidth = 1;
-    //gl.rect(buffer+i, buffer+j, size, size);
-    //gl.stroke();
-
-  //}
-    //}
-  //}
-
-//  gl.fillStyle = "white";
-//  gl.font = '72px helvetica';
-//  gl.fillText('Chelsea Analytics', rectWidth/16, rectHeight/2);
-//  window.requestAnimationFrame(main);
-//  gl.textAlign = 'left';
-//  var a = document.createElement('a');
-
-// Main Program
-
-
-//window.requestAnimationFrame(main);
+gl.save();
+var animator = window.setInterval(draw, 0.25);
