835ae6d2d1bab1056140365ed1c4a730741adde2
chelseaanalytics <79656978+chelseaanalytics@users.noreply.github.com>
2021-02-27 11:30:05 -0500

    Updates

--- chelsea.js
+++ chelsea.js
@@ -1,79 +1,124 @@
-function draw() {
+// function draw() {
+//   //Animate();
+//   Text();
+// }
+
+function Animate() {
 
 //Draw random fills
-a = Math.random()*rectWidth;
+a = Math.random()*RECTWIDTH;
 a = Math.round(a/size)*size;
-b = Math.random()*rectHeight;
+b = Math.random()*RECTHEIGHT;
 b = Math.round(b/size)*size;
 
-if((a+size)<=rectWidth && (b+size)<=rectHeight){
-  gl.fillStyle = '#064276';  //#073156
-  gl.fillRect(a, b, size, size);
-  gl.fillStyle = '#0E4778';  //#073156
-  gl.fillRect(a-size, b-size, size, size);
-  gl.fillStyle = '#073156';  //#073156 #CBE3F8
-  gl.fillRect(a, b-size, size, size);
-  //gl.fillStyle = '#CBE3F8';  //#073156 #CBE3F8
-  //gl.fillRect(a-size, b, size, size);
-  gl.strokeStyle = "#064276";
-  gl.lineWidth = 1;
-  gl.rect(a, b, size, size);
-  gl.stroke();
-  gl.save
-}
-else {
-  a = 0;
-  b = 0;
+if((a+size)<=RECTWIDTH && (b+size)<=RECTHEIGHT){
+  ctx1.fillStyle = '#064276';  //#073156
+  ctx1.fillRect(a, b, size, size);
+  ctx1.fillStyle = '#0E4778';  //#073156
+  ctx1.fillRect(a-size, b-size, size, size);
+  ctx1.fillStyle = '#073156';  //#073156 #CBE3F8
+  ctx1.fillRect(a, b-size, size, size);
+  //ctx1.fillStyle = '#CBE3F8';  //#073156 #CBE3F8
+  //ctx1.fillRect(a-size, b, size, size);
+  ctx1.strokeStyle = "#064276";
+  ctx1.lineWidth = 1;
+  ctx1.rect(a, b, size, size);
+  ctx1.stroke();
+  ctx1.save
+  // if ((COUNT % 100) === 0) {
+  //   ctx1.fillStyle = "white";
+  //   ctx1.font = '72px Helvetica';
+  //   ctx1.textAlign = 'center';
+  //   ctx1.fillText('Chelsea Analytics', RECTWIDTH/2, RECTHEIGHT/2);
+  // }
 }
 
-if (count<10000) {
-  count = count+1;
-}
+if (COUNT<5000) {
+  COUNT = COUNT+1;
+  }
 else {
+  //Print text again so it's on top of animation
+  ctx1.fillStyle = "White";
+  ctx1.font = '72px Helvetica';
+  ctx1.textAlign = 'center';
+  ctx1.fillText('Chelsea Analytics', RECTWIDTH/2, RECTHEIGHT/2);
   window.clearInterval(animator);
 }
 
 // Draw text
-gl.fillStyle = "white";
-if (width > 800) {
-  gl.font = '72px Helvetica';
-}
-else {
-  gl.font = '36px Helvetica';
-}
-gl.textAlign = 'center';
-gl.fillText('Chelsea Analytics', width/2, height/2);
+//ctx1.fillStyle = "white";
+//if (width > 800) {
+//  ctx1.font = '72px Helvetica';
+//}
+//else {
+//  ctx1.font = '36px Helvetica';
+//}
+//ctx1.textAlign = 'center';
+//ctx1.fillText('Chelsea Analytics', width/2, height/2);
 
 }
+
+function Text() {
+//ctx1.clearRect(0, 0, RECTWIDTH, RECTHEIGHT);
+
+// if (WIDTH > 800) {
+//   ctx1.font = '72px Helvetica';
+// }
+// else {
+//   ctx1.font = '36px Helvetica';
+// }
+ctx2.fillStyle = "White";
+ctx2.font = '72px Helvetica';
+ctx2.textAlign = 'center';
+ctx2.fillText('Chelsea Analytics', RECTWIDTH/2, RECTHEIGHT/2);
+
+}
+
 
 // *******************   Main program   ******************* //
-const canvas = document.querySelector("Canvas");
-var gl = document.getElementById('Canvas').getContext('2d');
 
 var x = 0;
 var y = 0;
 var size = 40;
 var buffer = 0;
-var count = 0;
+var COUNT = 0;
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+Animation = document.getElementById("Animation");
+ctx1 = Animation.getContext('2d');
 
-rectWidth = Math.floor((width/size))*size+buffer*2;
-rectHeight = Math.floor((height/size))*size+buffer*2;
+Text = document.getElementById("Text");
+ctx2 = Text.getContext('2d' , );
+ctx2.canvas.width = 1200;
+ctx2.canvas.height = 800;
+ctx2.canvas.left = 500;
 
-// Draw canvas
-gl.clearRect(0, 0, rectWidth, rectHeight);
-gl.fillStyle = '#BFDDF6';
-gl.fillRect(0, 0, rectWidth, rectHeight);
+var width = ctx1.canvas.width = 1200;
+var height = ctx1.canvas.height = 800;
 
-// Draw background
-for (x=0;x<rectWidth;x+=size) {
-  for (y=0;y<=rectHeight;y+=size) {
-    gl.rect(x, y, size, size);
-    gl.stroke();
-  }
+var width2 = ctx2.canvas.width;
+var height2 = ctx2.canvas.height;
+
+ctx2.fillStyle = "White";
+ctx2.font = '72px Helvetica';
+ctx2.textAlign = 'center';
+ctx2.fillText('Chelsea Analytics', width2/2, height2/2);
+
+
+RECTWIDTH = Math.floor((width/size))*size+buffer*2;
+RECTHEIGHT = Math.floor((height/size))*size+buffer*2;
+
+ctx1.clearRect(0, 0, RECTWIDTH, RECTHEIGHT);
+ctx1.fillStyle = '#BFDDF6';
+ctx1.fillRect(0, 0, RECTWIDTH, RECTHEIGHT);
+
+
+//Draw background
+for (x=0;x<RECTWIDTH;x+=size) {
+  for (y=0;y<RECTHEIGHT;y+=size) {
+   ctx1.rect(x, y, size, size);
+   ctx1.stroke();
+ }
 }
 
-gl.save();
-var animator = window.setInterval(draw, 0.25);
+//Text();
+var animator = window.setInterval(Animate, 0.25);
